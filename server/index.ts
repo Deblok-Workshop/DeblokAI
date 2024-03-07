9bc7bdc49ebe1b1cb0b3d5476f462b6bbc4ebd73
uhidontkno <hi@rare1k.dev>
2024-03-06 19:22:34 -0500

    chore: add webserver + init tailwind

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia";
+import { staticPlugin } from "@elysiajs/static";
+
+new Elysia()
+  .use(staticPlugin({ prefix: "/", assets: "./static" })) // dont cd into /server
+  .listen(process.env.PORT || 8080);
+
+console.log(`Listening on port ${process.env.PORT || 3000} or`);
+console.log(` │ 0.0.0.0:${process.env.PORT || 3000}`);
+console.log(` │ 127.0.0.1:${process.env.PORT || 3000}`);
+console.log(` └─────────────────────────>`);
