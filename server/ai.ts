c6a4bc704612a09ee9955439d73d3375f562fd0a
uhidontkno <hi@rare1k.dev>
2024-03-06 22:12:18 -0500

    chore: install prettier and run it

--- server/ai.ts
+++ server/ai.ts
@@ -1,26 +1,28 @@
 import OpenAI from "openai";
 const env = {
-    "OPENAI_API_KEY":process.env.OPENAI_API_KEY || "", 
-    "OPENAI_BASE_URL":process.env.OPENAI_BASE_URL || "https://api.openai.com/v1/"
-
-}
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
+  OPENAI_BASE_URL: process.env.OPENAI_BASE_URL || "https://api.openai.com/v1/",
+};
 const openai = new OpenAI();
 openai.apiKey = env.OPENAI_API_KEY;
 openai.baseURL = env.OPENAI_BASE_URL;
 
-async function generate(model:string,messages:Object,prompt:Object) {
- let msgs:any = [{ role: "system", content: `${prompt ? prompt : "You are a helpful assistant called DeblokAI."}` }];
- Object.assign({1: messages},msgs)
- let c = await openai.chat.completions.create({
+async function generate(model: string, messages: Object, prompt: Object) {
+  let msgs: any = [
+    {
+      role: "system",
+      content: `${prompt ? prompt : "You are a helpful assistant called DeblokAI."}`,
+    },
+  ];
+  Object.assign({ 1: messages }, msgs);
+  let c = await openai.chat.completions.create({
     messages: msgs,
     model: model || "gpt-3.5-turbo",
   });
-  console.log(msgs)
+  console.log(msgs);
   return c.choices;
 }
 
-
 export default {
-    generate:generate
-    
-}
+  generate: generate,
+};
--- server/index.ts
+++ server/index.ts
@@ -1,49 +1,59 @@
 import { Elysia } from "elysia";
 import { staticPlugin } from "@elysiajs/static";
-import ai from "./ai.ts"
-const server = new Elysia()
+import ai from "./ai.ts";
+const server = new Elysia();
 
-server.use(staticPlugin({ prefix: "/", assets: "./static" })) // dont cd into /server
+server.use(staticPlugin({ prefix: "/", assets: "./static" })); // dont cd into /server
 server.listen(process.env.PORT || 3000);
 
 const env = {
-    "OPENAI_API_KEY":process.env.OPENAI_API_KEY, 
-    "OPENAI_BASE_URL":process.env.OPENAI_BASE_URL || undefined
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  OPENAI_BASE_URL: process.env.OPENAI_BASE_URL || undefined,
+};
 
+if (!env.OPENAI_API_KEY) {
+  console.error("error: No API key present! Exiting.");
+  process.exit(1);
 }
 
-if (!env.OPENAI_API_KEY) {console.error("error: No API key present! Exiting.");process.exit(1)}
+server.get("/api", ({ set }) => {
+  set.redirect = "https://img.rare1k.dev/_broken_links.gif";
+});
+server.get("/api/", ({ set }) => {
+  set.redirect = "https://img.rare1k.dev/_broken_links.gif";
+});
 
-server.get("/api",({ set })=> {set.redirect = "https://img.rare1k.dev/_broken_links.gif"})
-server.get("/api/",({ set })=> {set.redirect = "https://img.rare1k.dev/_broken_links.gif"})
+server.post("/api/completions", async ({ set, body }) => {
+  let b: any = body; // elysia bug
+  try {
+    b = JSON.parse(b);
+  } catch {
+    set.status = 400;
+    return { error: "Invalid JSON" };
+  }
+  b.model = b.model ? b.model : "gpt-3.5-turbo";
+  b.prompt = b.prompt
+    ? b.prompt
+    : "You are a helpful assistant called DeblokAI.";
+  let r: any = await ai.generate(b.model, b.messages, b.prompt); // unsure what typeof but its ok
+  return r;
+});
 
-server.post("/api/completions",async ({ set,body }) => {
-    let b:any = body; // elysia bug
-    try {
-    b = JSON.parse(b)
-    } catch {
-        set.status = 400;
-        return {"error":"Invalid JSON"}
-    }
-    b.model = b.model ? b.model : "gpt-3.5-turbo";
-    b.prompt = b.prompt ? b.prompt : "You are a helpful assistant called DeblokAI.";
-    let r:any = await ai.generate(b.model,b.messages,b.prompt) // unsure what typeof but its ok
-    return r;
-})
-
-server.post("/api/chat",async ({ set,body }) => {
-    let b:any = body; // elysia bug
-    try {
-    b = JSON.parse(b)
-    } catch {
-        set.status = 400;
-        return {"error":"Invalid JSON"}
-    }
-    b.model = b.model ? b.model : "gpt-3.5-turbo";
-    b.prompt = b.prompt ? b.prompt : "You are a helpful assistant called DeblokAI.";
-    let r:any = await ai.generate(b.model,b.messages,b.prompt);
-    return r.message.content
-})
+server.post("/api/chat", async ({ set, body }) => {
+  let b: any = body; // elysia bug
+  try {
+    b = JSON.parse(b);
+  } catch {
+    set.status = 400;
+    return { error: "Invalid JSON" };
+  }
+  b.model = b.model ? b.model : "gpt-3.5-turbo";
+  b.prompt = b.prompt
+    ? b.prompt
+    : "You are a helpful assistant called DeblokAI.";
+  let r: any = await ai.generate(b.model, b.messages, b.prompt);
+  return r.message.content;
+});
 
 console.log(`Listening on port ${process.env.PORT || 3000} or`);
 console.log(` │ 0.0.0.0:${process.env.PORT || 3000}`);
