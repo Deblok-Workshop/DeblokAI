4ca610db586921e90d2b2cc594b49eb603d9fadc
uhidontkno <hi@rare1k.dev>
2024-03-06 19:28:00 -0500

    add env shit we need for later

--- server/index.ts
+++ server/index.ts
@@ -6,6 +6,13 @@ const server = new Elysia()
 server.use(staticPlugin({ prefix: "/", assets: "./static" })) // dont cd into /server
 server.listen(process.env.PORT || 3000);
 
+const env = {
+    "API_KEY":process.env.API_KEY, 
+    "OPENAI_BASE":process.env.OPENAI_BASE || undefined
+
+}
+
+if (!env.API_KEY) {console.error("error: No API key present! Exiting.");process.exit(1)}
 
 console.log(`Listening on port ${process.env.PORT || 3000} or`);
 console.log(` │ 0.0.0.0:${process.env.PORT || 3000}`);
