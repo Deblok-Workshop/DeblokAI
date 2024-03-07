26edb35c15a3145f913b070a70f70e5f35ed0051
uhidontkno <hi@rare1k.dev>
2024-03-06 19:40:46 -0500

    do some quick skidding

--- server/ai.ts
+++ server/ai.ts
@@ -1,5 +1,17 @@
-import openai from "openai";
+import OpenAI from "openai";
+const env = {
+    "API_KEY":process.env.API_KEY || "", 
+    "OPENAI_BASE":process.env.OPENAI_BASE || "https://api.openai.com/v1/"
+
+}
+const openai = new OpenAI();
+openai.apiKey = env.API_KEY;
+openai.baseURL = env.OPENAI_BASE;
 
 async function generate(model:string,messages:Object,prompt:Object) {
- throw new Error("not implemented");
+ let c = await openai.chat.completions.create({
+    messages: [{ role: "system", content: `${prompt ? prompt : "You are a helpful assistant called DeblokAI."}` }],
+    model: "gpt-3.5-turbo",
+  })
+  return c.choices[0];
 }
