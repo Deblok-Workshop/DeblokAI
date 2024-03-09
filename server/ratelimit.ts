fd9be35aa157f46194561c4a20d320e874548e59
rare1k <hi@rare1k.dev>
2024-03-09 00:31:14 -0500

    Add ratelimit config

---
+++ server/ratelimit.ts
@@ -0,0 +1,8 @@
+  export default {
+    duration: 300 * 1000,
+    max: 20,
+    responseMessage: "Global rate limit reached.",
+    skip: (req: any) => {
+      return !new URL(req.url).pathname.startsWith("/api");
+    },
+  },
